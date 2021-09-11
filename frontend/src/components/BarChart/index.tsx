aa0d1e7e72b94b069b53da80f38e074d8b4e7a0d
joaovmelo <joaovmelo.17@gmail.com>
2021-09-11 14:19:27 -0300

    Security config

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -24,11 +24,11 @@ const BarChart = () => {
 
 
     return (
-        <Chart 
-            options={{ ...options, xaxis:mockData.labels}}    
+        <Chart
+            options={{ ...options, xaxis: mockData.labels }}
             series={mockData.series}
             type="bar"
-            height="240"    
+            height="240"
         />
     );
 
